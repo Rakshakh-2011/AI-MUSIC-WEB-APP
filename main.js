e08ab3a32b0747bb864f784ef9343a4555b7c635
Rakshakh-2011 <78683334+Rakshakh-2011@users.noreply.github.com>
2021-12-03 15:49:35 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,16 +1,42 @@
 song ="";
 function preload(){
    song = loadSound()
+   leftWristX = 0;
+   leftWristY = 0;
+   rightWristX = 0;
+   rightWristY = 0;
 }
 function setup(){
 canvas = createCanvas(600, 500)
 canvas.center;
 video.createCapture(VIDEO);
 video.hide()
+poseNet = ml5.poseNet(video, modelloaded);
+posenet.on('pose', gotPoses)
+}
+function modelloaded(){
+    console.log('Posenet Is Initialised')
 }
 function draw(){
     image(video, 0, 0, 600, 500);
 }
 function play(){
     song.play();
+    song.setVolume(1);
+    song.rate(1);
 }
+function gotPoses(){
+    if(result.length > 0)
+    {
+        console.log(results);
+        leftWristX = results [0].pose.leftWristX.x;
+        leftWristY = results [0].pose.leftWristY.y;
+        console.log("leftWristX = ", + leftWristX + "leftWristY = ", + leftWristY );
+        rightWristX = results [0].pose.rightWristX.x;
+        rightWristY = results [0].pose.rightWristY.y;
+        console.log("rightWristX = ", + rightWristX + "rightWristY = ", + rightWristY );
+    }
+    
+    
+}
+
